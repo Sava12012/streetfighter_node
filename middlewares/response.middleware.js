1f563f93e5a9fe79759664ec5a278c6e75c14ef6
Dmytro Savych <sava.savych@gmail.com>
2023-06-25 20:38:19 +0300

    + update responseMiddleware

--- middlewares/response.middleware.js
+++ middlewares/response.middleware.js
@@ -4,16 +4,16 @@ const responseMiddleware = (req, res, next) => {
     res.status(200).json(data);
   };
 
-  res.badRequest = (message) => {
-    res.status(400).json({ error: message });
+  res.badRequest = (errorMessage) => {
+    res.status(400).json({ error: errorMessage });
   };
 
-  res.notFound = (message) => {
-    res.status(404).json({ error: true, message: message });
+  res.notFound = (errorMessage) => {
+    res.status(404).json({ error: true, message: errorMessage });
   };
 
-  res.error = (message) => {
-    res.status(500).json({ error: message });
+  res.error = (errorMessage) => {
+    res.status(500).json({ error: errorMessage });
   };
   next();
 };
